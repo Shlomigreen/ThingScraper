6c7befb8ee3538984bea4dcc9c3b6b5405d7aa74
Konstantin Krivokon <konstantin_kri@protonmail.com>
2021-02-21 17:56:11 +0200

    removed redundancy from main file, now works exclusively with ThingScraper methods. Removed implicit wait, now waits for 20 elements to load per page explicitly.

--- config.py
+++ config.py
@@ -3,11 +3,14 @@
 # chrome webdriver: https://sites.google.com/a/chromium.org/chromedriver/downloads
 
 
+
+
+
 # TEST CONFIGS
+
 
 
 # CHANGEABLE CONFIGS
 parser = 'lxml'  # specific parser ("lxml", "lxml-xml", "html.parser", or "html5lib") or the type of markup to be used ("html", "html5", "xml")
-browser = "chrome"  # the browser to be used
-driver_path = r"chromedriver"  # the browser's driver to be used
-get_wait_timeout = 10  # timeout for requesting full site in seconds
+driver_path = r"A:\Users\Danik\Desktop\evrything and beyond\ITC\projects\datamining\chromedriver.exe"
+get_wait_timeout = 10  # in seconds
--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from ThingScraper import Browser, Thing
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
 import config as conf
-import os
-import time
+from selenium.webdriver.common.by import By
 
-full_driver_path = os.path.abspath(conf.driver_path)
 BROWSER_TYPE = "chrome"
 URL = "https://www.thingiverse.com/"
 URL_FRONT = "https://www.thingiverse.com/"
@@ -18,40 +10,12 @@ ELEMENT_LINK = "ThingCardBody__cardBodyWrapper--ba5pu"
 ELEMENT_PROJECT = "ThingCard__thingCard--1IcHY"
 ELEMENT_NEXT_PAGE = "Pagination__button--2X-2z Pagination__more--24exV"
 TIMEOUT_TOLERANCE = 5
+THINGS_PER_PAGE = 20
 
 PAGES_TO_SCAN = 500
 TEST = r"https://www.thingiverse.com/search?page=3&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
 TEST1 = r"search?page="
 TEST2 = r"&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
-
-
-
-URL_THING_4734271 = "https://www.thingiverse.com/thing:4734271"
-
-
-def get_elements(browser, *args):
-    """
-    Scans a webpage for the provided element names, and returns a list of the results
-    :param browser: A web browser loaded with the web page we're scraping
-    :param args: The elements we're looking for in the page
-    :return: A list of lists, where each element corresponds to an input argument
-    """
-    res = []
-    for element in args:
-        found_elements = None
-        try:
-            WebDriverWait(browser.driver, TIMEOUT_TOLERANCE).until(
-                EC.presence_of_element_located((By.CLASS_NAME, element))
-            )
-            time.sleep(4)
-            found_elements = browser.driver.find_elements_by_class_name(element)
-        except TimeoutException:
-            # TODO: Handle exceptions
-            pass
-        finally:
-            res.append(found_elements)
-        time.sleep(4)
-    return res
 
 
 def scraper_search(browser, pages_to_scan=PAGES_TO_SCAN):
@@ -65,9 +29,12 @@ def scraper_search(browser, pages_to_scan=PAGES_TO_SCAN):
     for i in range(pages_to_scan):
         url = URL + TEST1 + str(i+1) + TEST2
         browser.get(url)
-        projects = get_elements(browser, ELEMENT_PROJECT)
-        print(f"Found {len(projects[0])} projects on page {i+1}")
-        for item in projects[0]:
+        projects = []
+        while len(projects) < THINGS_PER_PAGE:
+            projects = browser.wait_and_find(By.CLASS_NAME, ELEMENT_PROJECT, find_all=True)
+        print(f"Found {len(projects)} projects on page {i+1}")
+        for item in projects:
+            # TODO: We might need to add support for selenium stale element exception/ wait
             item_id = item.find_element_by_class_name("ThingCardBody__cardBodyWrapper--ba5pu").get_attribute("href")
             item_id = item_id.rsplit(':', 1)[1]
             likes = item.find_elements_by_class_name("CardActionItem__textWrapper--2wTM-")[1]
@@ -78,20 +45,21 @@ def scraper_search(browser, pages_to_scan=PAGES_TO_SCAN):
 
 
 def main():
-    browser = Browser(BROWSER_TYPE, full_driver_path)
-    data = scraper_search(browser, 5)
-    for key in data:
-        # print(f"For item {key}:\n{data[key].properties}\n")
-        print(data[key])
-    browser.close()
-    # browser = Browser(conf.browser, conf.driver_path)
-    # try:
-    #     thing = Thing(URL_THING_4734271)
-    #     thing.fetch_all(browser)
-    #     print("Model Name:", thing.elements['model_name'].text)
-    #     print("Created", thing.elements['created_by'].text)
-    # finally:
-    #     browser.close()
+    browser = Browser(BROWSER_TYPE, conf.driver_path)
+    try:
+        data = scraper_search(browser, 30)
+        failed = []
+        for key in data:
+            try:
+                # data[key].parse_all(browser)
+                pass
+            except Exception as E:
+                failed.append((key, E))
+                print(f"Failed to retrieve for item id = {key}\n")
+            else:
+                print(data[key])
+    finally:
+        browser.close()
 
 
 if __name__ == '__main__':
