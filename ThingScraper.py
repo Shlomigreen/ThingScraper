b02a7f82426cfe03e3488263f742d3dcd931032e
Konstantin Krivokon <konstantin_kri@protonmail.com>
2021-02-22 15:27:45 +0200

    Updated to branch

--- ThingScraper.py
+++ ThingScraper.py
@@ -9,6 +9,7 @@ import os
 import re
 import datetime
 
+# Url of the main domain
 URL = "https://www.thingiverse.com/"
 
 
@@ -29,80 +30,137 @@ class Thing:
     """
 
     def __init__(self, **kwargs):
-        if kwargs.get('url') is not None :
+        """
+        Creating a new thing instance. Must provide either thing id or url as arguments.
+        """
+        # Validating construction arguments and adding them as instance arguments
+        if kwargs.get('url') is not None:
             self.url = kwargs['url']
             self.thing_id = kwargs['url'].split(sep=":")[-1]
-        elif kwargs.get('id') is not None :
+        elif kwargs.get('id') is not None:
             self.thing_id = kwargs['id']
             self.url = URL + "thing:" + kwargs['id']
-        else :
-            raise ValueError("Input must be either id or url")
+        # in case neither id nor url were given, raise a value error.
+        else:
+            raise ValueError(
+                "Construction arguments for thing object must include either an id or a url. Given arguments:",
+                **kwargs)
+
         # Declaring empty dictionaries to hold page elements and properties (parsed elements)
         self._elements = dict()
-        self.properties = dict()
+        self.properties = kwargs.get('properties') if not None else dict()
+        # self.properties = dict()
 
-    def __getitem__(self, item) :
+    def __getitem__(self, item):
+        """
+        Implementation of special function to allow retrieving a thing parameter by using squared brackets.
+       :param item: key for the requested thing property.
+       :return: thing property.
+        """
         return self.properties.get(item)
 
-    def __setitem__(self, key, value) :
+    def __setitem__(self, key, value):
+        """
+        Implementation of special function to allow setting a thing parameter by using squared brackets.
+       :param key: key for requested thing property
+       :param value: the value to set for the property
+        """
         self.properties[key] = value
 
-    def __add__(self, other) :
-        if isinstance(other, dict) :
+    def __add__(self, other):
+        """
+        Implementation of special function to allow appending parameters to a thing by using '+' sign.
+        Appended object must be of type dict.
+       :param other: dictionary with keys as properties.
+       :return: self
+        """
+
+        # verify given parameter to be of type dict
+        if isinstance(other, dict):
             self.properties.update(other)
         else:
-            raise TypeError(f"Can only add dictionary, not {type(other)}")
+            raise TypeError(f"Can only add dictionary, not {type(other)}.")
         return self
 
-    def __str__(self) :
-        output = f"Thing number {self.thing_id}:\n"
-        for key in self.properties :
-            output += f"\t{key} = {self.properties[key]}\n"
-        return output
+    def __str__(self):
+        """
+        Implementation of special function to allow str casting of thing instance.
+       :return: thing's url
+        """
+        return self.url
 
-    def keys(self) :
-        return self.properties.keys()
+    def __repr__(self):
+        """
+        Implementation of special function to allow representation of a thing instance.
+       :return: thing's url
+        """
+        return self.url
 
-    def fetch_all(self, browser) :
+    def print_info(self):
+        """
+        Prints full information about the thing instance.
+        """
+        output = [f"Thing number {self.thing_id}",
+                  self.url]
+        for key in self.properties:
+            output.append(f"\t{key} = {self.properties[key]}")
+        print("\n".join(output))
+
+    def keys(self):
+        """
+        Returns all property names held for the thing instance.
+        """
+        return tuple(self.properties.keys())
+
+    def fetch_all(self, browser):
+        """
+        Breaks down the thing's url into elements (tags and classes) holding properties.
+       :param browser: Browser object the be used for accessing the thing's url.
+        """
+
         # open thing url
         browser.get(self.url)
 
-        # thing model_name
+        # obtain element holding the model (thing) name
         self._elements['model_name'] = browser.wait_and_find(By.CLASS_NAME, "ThingPage__modelName--3CMsV")
 
-        # created by
+        # obtain element holding both the creator name and the uploaded date
         self._elements['created_by'] = browser.wait_and_find(By.CLASS_NAME, "ThingPage__createdBy--1fVAy")
 
-        # tab buttons
+        # obtain tab buttons holding metric information: files, comments, makes and remixes
         all_metrics = browser.wait_and_find(By.CLASS_NAME, "MetricButton__tabButton--2rvo1", find_all=True)
         self._elements['tab_buttons'] = {
-            metric.find_element_by_class_name("MetricButton__tabTitle--2Xau7") : metric.find_element_by_class_name(
+            metric.find_element_by_class_name("MetricButton__tabTitle--2Xau7"): metric.find_element_by_class_name(
                 "MetricButton__metric--FqxBi")
             for metric in all_metrics}
 
-        # tags
+        # obtain all tag elements into a list
         all_tags = browser.wait_and_find(By.CLASS_NAME, "Tags__widgetBody--19Uop")
-        try :
+        try:
             self._elements['tags'] = [tag for tag in all_tags.find_elements_by_tag_name('a')]
-        except NoSuchElementException :
+        except NoSuchElementException:
             self._elements['tags'] = None
 
-        # print settings
-        # TODO: Not all models has this, check what happens in a model without print settings
-        try :
+        # obtain print settings element
+        # this is an optional information the creator can provide, so some models may not have this information.
+        try:
+            # wait for element to be available
             browser.wait(By.CLASS_NAME, "ThingPage__blockTitle--3ZdLu")
+            # search for a div tag with certain class that hold the text for print settings
             print_settings = browser.driver.find_element_by_xpath(
                 '//div[@class="ThingPage__blockTitle--3ZdLu" and text()="Print Settings"]')
+            # obtain parent tag in order to access actual print settings
             print_settings_parent = print_settings.find_element_by_xpath('..')
+            # each provided setting appears in a new p tag so save all of them into elements
             self._elements['print_settings'] = print_settings_parent.find_elements_by_tag_name('p')
-        except NoSuchElementException :
+        except NoSuchElementException:
             self._elements['print_settings'] = None
 
-        # License
+        # Model License
         self._elements['license'] = browser.driver.find_element_by_xpath(
             "//a[@class='License__link--NFT8l' and not(@class='License__creator--4riPo')]")
 
-    def parse_all(self, clear_cache=True) :
+    def parse_all(self, clear_cache=True):
         """Obtain information from elements previously fetched for the thing.
 
                Parameters:
@@ -111,24 +169,31 @@ class Thing:
         # model name
         self.properties['model_name'] = self._elements['model_name'].text
 
-        # created by div
+        # get username found inside a tag text
         self.properties['creator_username'] = self._elements['created_by'].find_element_by_tag_name('a').get_attribute(
             'text')
+        # get username profile url provided as a tag href
         self.properties['creator_url'] = self._elements['created_by'].find_element_by_tag_name('a').get_attribute(
             'href')
 
-        # uploaded date
+        # use created by html to obtain uploaded date text (uploaded date appears after a end tag)
         date_text = self._elements['created_by'].get_attribute('innerHTML').split(sep='</a> ')[1]
+        # convert string date into actual date using datetime package
+        # TODO: Handle datetime
+        #self.properties['upload_date'] = datetime.datetime.strptime(date_text, "%B %d, %Y")
+        # self.properties['upload_date'] = date_text
         self.properties['upload_date'] = datetime.datetime.strptime(date_text, "%B %d, %Y")
 
-        # tab buttons
+        # Metric information
+        # set tab buttons to be ignore (hold not useful information)
         ignore_buttons = ('Thing Details', 'Apps')
-        for key, value in self._elements['tab_buttons'].items() :
-            if key.text not in ignore_buttons :
+        # for each tab button element, add it's name (converted using field_it function) and value to properties
+        for key, value in self._elements['tab_buttons'].items():
+            if key.text not in ignore_buttons:
                 # using tab button names as field names. lowering case and replacing spaces with underscore
                 self.properties[field_it(key.text)] = value.text
 
-        # tags
+        # Obtain text from each tag element add add them all as a list to properties
         self.properties['tags'] = [tag.text for tag in self._elements['tags']]
 
         # print settings
@@ -143,24 +208,24 @@ class Thing:
                                    "Filament Color",
                                    "Filament Material"]
 
-        # add empty print settings to properties
+        # add empty print settings to properties (as some models may not have any print settings information
         self.properties.update({field_it(key): None for key in possible_print_settings})
 
-        # define regex search patter to obtain setting and its value
-        pattern = "(.*):<div>(.*)<\/div>"
-        for setting in self._elements['print_settings']:
-            # using regex to obtain setting name and value into two groups
-            regex_result = re.search(pattern, setting.get_attribute('innerHTML'))
+        if self._elements['print_settings'] is not None:
+            # define regex search patter to obtain setting and its value
+            pattern = r"(.*):<div>(.*)</div>"
+            for setting in self._elements['print_settings']:
+                # using regex to obtain setting name and value into two groups
+                regex_result = re.search(pattern, setting.get_attribute('innerHTML'))
 
-            if regex_result is not None:
-                # updating print settings (group 1 or regex resutls) with its value (group 2 of regex results) into the properties
-                self.properties.update({field_it(regex_result.group(1)): regex_result.group(2)})
-
+                if regex_result is not None:
+                    # updating print settings (group 1 or regex resutls) with its value (group 2 of regex results) into the properties
+                    self.properties.update({field_it(regex_result.group(1)): regex_result.group(2)})
 
         # license
         self.properties['license'] = self._elements['license'].text
 
-        if clear_cache :
+        if clear_cache:
             self._elements.clear()
 
 
@@ -172,10 +237,10 @@ class Browser:
     """
 
     # Dictionary that defines browsers and their relevant webdriver class
-    available_browsers = {'chrome' : webdriver.Chrome, 'firefox' : webdriver.Firefox, 'iexplorer' : webdriver.Ie,
-                          'safari' : webdriver.Safari}
+    available_browsers = {'chrome': webdriver.Chrome, 'firefox': webdriver.Firefox, 'iexplorer': webdriver.Ie,
+                          'safari': webdriver.Safari}
 
-    def __init__(self, name, path) :
+    def __init__(self, name, path):
         """Construction of a new browser instance
 
                Parameters:
@@ -185,43 +250,81 @@ class Browser:
         self.name = name
         self.driver_path = os.path.abspath(path)
 
-        if self.name in Browser.available_browsers :
+        if self.name in Browser.available_browsers:
             self.driver = Browser.available_browsers[name](self.driver_path)
-        else :
+        else:
             raise ValueError(
                 f"Requested browser '{name}' not available. Usable browsers:\n {list(Browser.available_browsers.keys())}")
 
         # minimise the opened browser
         self.driver.minimize_window()
 
-    def get(self, url) :
+    def __enter__(self):
+        """
+        Allows to open browser connection using 'with' statement
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Allows to close browser connection using 'with' statment
+        """
+        self.close()
+
+    def get(self, url):
         """
         Equivalent to Browser.driver.get method
         """
 
         self.driver.get(url)
 
-    def close(self) :
+    def close(self):
         """
         Equivalent to Browser.driver.close method
         """
         self.driver.close()
 
-    def wait(self, by, name, timeout=conf.get_wait_timeout, regex=False) :
-        # TODO: implement in wait_and_find
+    def wait(self, by, name, timeout=conf.get_wait_timeout, regex=False):
+        """
+        Wait for specific element to be present in browser.
+
+            Parameters:
+                by (selenium.webdriver.common.by): html tag attribute to search for
+                name (str): the 'by' value to search for
+                timeout (int): time limit in seconds to wait for find values 'by' and 'name' to appear on page. Defualt: get_wait_timout on config.py
+                regex (bool): if true, regex search patterns are enabled for 'name'.
+        """
         # change the search name to re.compile of regex is enabled
-        if regex :
+        if regex:
             name = re.compile(name)
 
-        # wait for given element to be available and then retrieve it
-        # usage: wait_and_find(driver, By.CLASS_NAME, 'class_name')
+        # wait for given element to be available. raise error if timeout has been reached.
         WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located((by, name))
         )
 
-    def wait_and_find(self, by, name, timeout=conf.get_wait_timeout, find_all=False, regex=False) :
-        # TODO: Update docstring
+    def find(self, by, name, find_all=False):
+        """
+        Find specific element in browser.
+        Equivalent to Browser.driver.find_element / Browser.driver.find_elements
+
+            Parameters:
+                by (selenium.webdriver.common.by): html tag attribute to search for
+                name (str): the 'by' value to search for
+                find_all (bool): if true, returned value will be a list of all found elements. Default: False
+
+            Returns:
+                (webdriver.remote.webelement.WebElement): the found element(s)
+        """
+        # return found element(s) depends
+        if find_all:
+            return self.driver.find_elements(by, name)
+        else:
+            return self.driver.find_element(by, name)
+
+    def wait_and_find(self, by, name, timeout=conf.get_wait_timeout, find_all=False, regex=False):
         """Wait for given element in the opened page on the browser and return the searched result.
+           Combination of Browser.wait and Browser.find methods.
 
                Parameters:
                 by (selenium.webdriver.common.by): html tag attribute to search for
@@ -231,36 +334,19 @@ class Browser:
                 regex (bool): if true, regex search patterns are enabled for 'name'.
 
                Returns:
-                (webdriver.remote.webelement.WebElement) : the found element(s)
+                (webdriver.remote.webelement.WebElement): the found element(s)
         """
-        # change the search name to re.compile of regex is enabled
-        if regex :
-            name = re.compile(name)
-
-        # wait for given element to be available and then retrieve it
-        # usage: wait_and_find(driver, By.CLASS_NAME, 'class_name')
-        WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_element_located((by, name))
-        )
-
-        # return found element(s) depends
-        if find_all :
-            return self.driver.find_elements(by, name)
-        else :
-            return self.driver.find_element(by, name)
-
-    # TODO: add open with functionallity
+        self.wait(by, name, timeout, regex)
+        return self.find(by, name, find_all)
 
 
-
-def main() :
-    browser = Browser(conf.browser, conf.driver_path)
-    thing = Thing(id='4734271')
-    thing.fetch_all(browser)
-    thing.parse_all()
-    print("Done")
-    browser.close()
+def main():
+    with Browser(conf.browser, conf.driver_path) as browser:
+        thing = Thing(id='5')
+        thing.fetch_all(browser)
+        thing.parse_all()
+        thing.print_info()
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ThingScraper import Browser, Thing
 import config as conf
 from selenium.webdriver.common.by import By
+import json
 
 BROWSER_TYPE = "chrome"
 URL = "https://www.thingiverse.com/"
@@ -9,13 +10,54 @@ ELEMENT_NAME = "ThingCard__thingCard--1IcHY"
 ELEMENT_LINK = "ThingCardBody__cardBodyWrapper--ba5pu"
 ELEMENT_PROJECT = "ThingCard__thingCard--1IcHY"
 ELEMENT_NEXT_PAGE = "Pagination__button--2X-2z Pagination__more--24exV"
+ELEMENT_LIKES = "CardActionItem__textWrapper--2wTM-"
 TIMEOUT_TOLERANCE = 5
 THINGS_PER_PAGE = 20
-
 PAGES_TO_SCAN = 500
-TEST = r"https://www.thingiverse.com/search?page=3&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
-TEST1 = r"search?page="
-TEST2 = r"&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
+URL_SEARCH_EXAMPLE = r"https://www.thingiverse.com/search?page=3&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
+SEARCH_PARAM_FRONT = r"search?page="
+SEARCH_PARAM_END = r"&per_page=20&sort=popular&posted_after=now-30d&type=things&q="
+NEW_FILE = True
+
+
+def save_file(file_path, things_dict):
+    """
+    Saves the file
+    :param file_path: where to save the file (includes name)
+    :param things_dict: A dictionary where the key is the id, and the value is a Thing object
+    :return: (bool) True if saved successfully
+    """
+    state = False
+    try:
+        with open(file_path, 'w') as file:
+            data = [(id, things_dict[id].properties) for id in things_dict]
+            json.dump(data, file)
+    except Exception as E:
+        print(f"Could not save the file:\n{type(E)}: {E}")
+    else:
+        state = True
+    finally:
+        return state
+
+
+def open_file(file_path):
+    """
+    Opens saved file
+    :param file_path: where to save the file (includes name)
+    :return: A dict where the key is a thing id, and the value is a Thing object
+    """
+    res = None
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError as E:
+        print(f"File {file_path} not found:\n{E}")
+    except Exception as E:
+        print(f"Could not open the file:\n{E}")
+    else:
+        res = {key: Thing(id=key, properties=val) for key, val in data}
+    finally:
+        return res
 
 
 def scraper_search(browser, pages_to_scan=PAGES_TO_SCAN):
@@ -27,39 +69,41 @@ def scraper_search(browser, pages_to_scan=PAGES_TO_SCAN):
     """
     data = []
     for i in range(pages_to_scan):
-        url = URL + TEST1 + str(i+1) + TEST2
+        url = URL + SEARCH_PARAM_FRONT + str(i + 1) + SEARCH_PARAM_END
         browser.get(url)
         projects = []
         while len(projects) < THINGS_PER_PAGE:
             projects = browser.wait_and_find(By.CLASS_NAME, ELEMENT_PROJECT, find_all=True)
         print(f"Found {len(projects)} projects on page {i+1}")
         for item in projects:
-            # TODO: We might need to add support for selenium stale element exception/ wait
-            item_id = item.find_element_by_class_name("ThingCardBody__cardBodyWrapper--ba5pu").get_attribute("href")
+            item_id = item.find_element_by_class_name(ELEMENT_LINK).get_attribute("href")
             item_id = item_id.rsplit(':', 1)[1]
-            likes = item.find_elements_by_class_name("CardActionItem__textWrapper--2wTM-")[1]
+            likes = item.find_elements_by_class_name(ELEMENT_LIKES)[1]
             thing = Thing(id=item_id)
             thing['likes'] = int(likes.text)
-            data.append((int(item_id), thing))
+            data.append((item_id, thing))
     return dict(data)
 
 
 def main():
-    browser = Browser(BROWSER_TYPE, conf.driver_path)
-    try:
-        data = scraper_search(browser, 30)
-        failed = []
+    if NEW_FILE:
+        with Browser(conf.browser, conf.driver_path) as browser:
+            data = scraper_search(browser, 1)
+            failed = []
+            for key in data:
+                try:
+                    data[key].fetch_all(browser)
+                    data[key].parse_all()
+                except Exception as E:
+                    failed.append((key, E))
+                    print(f"Failed to retrieve for item id = {key}\n")
+                else:
+                    print(data[key].print_info())
+            save_file("save.json", data)
+    else:
+        data = open_file("save.json")
         for key in data:
-            try:
-                # data[key].parse_all(browser)
-                pass
-            except Exception as E:
-                failed.append((key, E))
-                print(f"Failed to retrieve for item id = {key}\n")
-            else:
-                print(data[key])
-    finally:
-        browser.close()
+            print(data[key].print_info())
 
 
 if __name__ == '__main__':
