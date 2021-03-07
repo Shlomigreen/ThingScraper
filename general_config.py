94b39d76fcd6ba8519fb0c906a6d6186bc44e91f
Shlomi <shlomi.a7@gmail.com>
2021-03-07 22:29:26 +0200

    Makes implementation - Implementing Make object for scrapping make pages. - Several changes to general_config.py - Implementing Thing object as ScrapedData child. **Note:** new object must be given either 'thing_id' or 'url' to be created.

--- general_config.py
+++ general_config.py
@@ -5,35 +5,13 @@ PAGES_TO_SCAN = 30  # Default value for number of pages to scan
 PLACE_HOLDER = '%%'
 
 
-class ThingSettings :
-    BASE_URL = f"https://www.thingiverse.com/thing:{PLACE_HOLDER}"
-
-    # Classes
-    MODEL_NAME = "ThingPage__modelName--3CMsV"
-    CREATED_BY = "ThingPage__createdBy--1fVAy"
-    TAB_BUTTON = "MetricButton__tabButton--2rvo1"
-    TAB_TITLE = "MetricButton__tabTitle--2Xau7"
-    METRIC = "MetricButton__metric--FqxBi"
-    TAG_LIST = "Tags__widgetBody--19Uop"
-    TAG_SINGLE = "Tags__tag--2Rr15"
-    PRINT_SETTINGS = "ThingPage__preHistory--312bi"
-    PRINT_SETTING = "ThingPage__description--14TtH"
-    REMIX_SECTION = "RemixedFromSection__title--1Wb7x"
-    REMIX_CARD = "ThingCardBody__cardBodyWrapper--ba5pu"
-    CATEGORY_SECTION = "ThingsMoreSection__showMoreHeading--u2OAR"
-    CATEGORY_NAME = "ThingsMoreSection__categoryName--3RWut"
-
-    # HTML paths
-    LICENSE_PATH = r"//a[@class='License__link--NFT8l' and not(@class='License__creator--4riPo')]"
-
-
-class ExploreList:
+class ExploreList :
     THING_CARD = "ThingCard__thingCard--1IcHY"
     CARD_BODY = "ThingCardBody__cardBodyWrapper--ba5pu"
     THING_LIKES = "CardActionItem__textWrapper--2wTM-"
 
 
-class UserSettings:
+class UserSettings :
     # Urls
     BASE_URL = f"https://www.thingiverse.com/{PLACE_HOLDER}/designs"
 
@@ -54,6 +32,66 @@ class UserSettings:
     ABOUT_WIDGET_TITLE = "UserAboutWidget__typesWrapper--1r1kj"
     ABOUT_WIDGET_SKILL = "UserAboutWidget__skillLevelWrapper--3eHjx"
 
-class MakeSettings:
+
+class MakeSettings :
     BASE_URL = f"https://www.thingiverse.com/make:{PLACE_HOLDER}"
     ID_REGEX = r"make:(\d*)"
+
+    POSSIBLE_PRINT_SETTINGS = ["Printer Brand",
+                                "Printer Model",
+                                "Rafts",
+                                "Supports",
+                                "Resolution",
+                                "Infill",
+                                "Filament"]
+
+
+    # Classes
+    SOURCE = "card-img-holder"
+    PAGE_INFO = "item-page-info"  ## Made by <username>, uploaded <time>
+    INFO_CONTENT = "thing-info-content"  ## Print settings section
+    SINGLE_PRINT_SETTING = "detail-setting"
+
+    # LIKES, COMMENTS and SHARES
+    POSSIBLE_ICONS = ['like', 'comment', 'share']
+    METRIC_ITEM_PATH = "//div[@class='item-list-interactions' and @data-make-id='{make_id}']//a[@title='{icon_title}']"
+
+    # VIEWS AND CATEGORY
+    MAKE_INFO = "//h2[@class='section-header']"
+    VIEWS = 'icon-views'
+    VIEWS_REGEX = r'(\d*) Views'
+    CATEGORY = 'icon-category'
+    CATEGORY_REGEX = r"Found in (.*)"
+
+
+class ThingSettings :
+    BASE_URL = f"https://www.thingiverse.com/thing:{PLACE_HOLDER}"
+    ID_REGEX = r"thing:(\d*)"
+
+    POSSIBLE_PRINT_SETTINGS = ["Printer Brand",
+                                "Printer Model",
+                                "Rafts",
+                                "Supports",
+                                "Resolution",
+                                "Infill",
+                                "Filament Brand",
+                                "Filament Color",
+                                "Filament Material"]
+
+    # Classes
+    MODEL_NAME = "ThingPage__modelName--3CMsV"
+    CREATED_BY = "ThingPage__createdBy--1fVAy"
+    TAB_BUTTON = "MetricButton__tabButton--2rvo1"
+    TAB_TITLE = "MetricButton__tabTitle--2Xau7"
+    METRIC = "MetricButton__metric--FqxBi"
+    TAG_LIST = "Tags__widgetBody--19Uop"
+    TAG_SINGLE = "Tags__tag--2Rr15"
+    REMIX_SECTION = "RemixedFromSection__title--1Wb7x"
+    REMIX_CARD = "ThingCardBody__cardBodyWrapper--ba5pu"
+    CATEGORY_SECTION = "ThingsMoreSection__showMoreHeading--u2OAR"
+    CATEGORY_NAME = "ThingsMoreSection__categoryName--3RWut"
+    PRINT_SETTINGS = "ThingPage__preHistory--312bi"
+    PRINT_SETTING = "ThingPage__description--14TtH"
+
+    # HTML paths
+    LICENSE_PATH = r"//a[@class='License__link--NFT8l' and not(@class='License__creator--4riPo')]"
